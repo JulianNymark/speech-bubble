b1e8d3a539b24859ef16ce238f62f05f74f95fed
Julian Nymark <julian.nymark@nav.no>
2024-12-03 17:03:35 +0100

    more tests

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,29 @@
+import { ReactNode } from "react";
 import { SpeechBubbleTail } from "./components/SpeechBubbleTail";
 import "./speech-bubble.css";
 
+const SpeechBubble = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex-initial speech-bubble max-w-80">
+      <div className="speech-bubble__tail">
+        <SpeechBubbleTail />
+      </div>
+      <div className="speech-bubble__content">{children}</div>
+    </div>
+  );
+};
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-[100vh] main-bg">
-      <div className="flex-initial speech-bubble w-80">
-        <div className="speech-bubble__tail">
-          <SpeechBubbleTail />
-        </div>
-        <div className="speech-bubble__content">
-          This is a border-image speech bubble test, it's a really good test. To
-          have a test like this is a good thing indeed. If you're not sure if
-          this is a good test or not, make sure to take a good look at it, and
-          judge it as hard as you possibly can.
-        </div>
-      </div>
+    <div className="flex flex-col gap-20 items-center justify-center h-[100vh] main-bg">
+      <SpeechBubble>
+        This is a border-image speech bubble test, it's a really good test. To
+        have a test like this is a good thing indeed. If you're not sure if this
+        is a good test or not, make sure to take a good look at it, and judge it
+        as hard as you possibly can.
+      </SpeechBubble>
+      <SpeechBubble>A smaller test of the speech bubble</SpeechBubble>
+      <SpeechBubble>A</SpeechBubble>
     </div>
   );
 }
